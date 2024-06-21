2ca8982a04028d1952d06bf959dacf8b42d8f038
LordBeans75 <jusmckay95@gmail.com>
2024-06-21 16:51:16 -0400

    maybe this works

--- src/main.jsx
+++ src/main.jsx
@@ -6,19 +6,19 @@ import Schedule from './components/Schedule';
 import About from './components/About';
 import Contact from './components/Contact';
 import './index.css';
-import './styles.css'; // Import global styles
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/schedule" />} />
         <Route path="/" element={<App />}>
+          <Route index element={<Navigate to="/schedule" />} />
           <Route path="schedule" element={<Schedule />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Route>
-        <Route path="*" element={<Navigate to="/schedule" />} /> {/* Catch-all route */}
+        <Route path="*" element={<Navigate to="/schedule" replace />} /> {/* Catch-all route */}
       </Routes>
     </Router>
   </React.StrictMode>
